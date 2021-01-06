5645315c52ae3c3e45dd3066e77b2d5120570ae2
Michelle <Minoord18@outlook.com>
2021-01-06 12:00:20 +0100

    Les 4 opdracht meet DOM ingeleverd

---
+++ les 4/B2L4MeetTheDOM/extern.js
@@ -0,0 +1,8 @@
+function clickedMe(){
+    document.getElementById("greet").textContent = "Thank you for using the DOM externally";
+}
+
+function clickedMeToo(){
+    document.getElementById("Title").textContent = "Meet The DOM";
+}
+
